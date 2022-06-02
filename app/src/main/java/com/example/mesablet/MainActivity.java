d862fd37ebbc0423cd397bd76d3ef67c8d321c40
koralduel <koralduel3112@gmail.com>
2022-06-02 16:50:23 +0300

    first page UI

--- app/src/main/java/com/example/mesablet/MainActivity.java
+++ app/src/main/java/com/example/mesablet/MainActivity.java
@@ -2,13 +2,16 @@ package com.example.mesablet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button Btn_start;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +20,13 @@ public class MainActivity extends AppCompatActivity {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
 
-        myRef.setValue("Hello, World!");
+        myRef.setValue("Hello");
+
+            Btn_start=findViewById(R.id.Btn_strat);
+            Btn_start.setOnClickListener(view -> {
+                Intent intent=new Intent(this,LoginPage.class);
+                startActivity(intent);
+            });
+        }
     }
 }

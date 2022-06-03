28e36d5c89ca1abcdbb5024d62120ea028a5a07b
Eynav Ptia <onevptaya@gmail.com>
2022-06-03 19:14:51 +0300

    login + fragment ui

--- app/src/main/java/com/example/mesablet/LoginPage.java
+++ app/src/main/java/com/example/mesablet/LoginPage.java
@@ -1,15 +1,30 @@
 package com.example.mesablet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class LoginPage extends AppCompatActivity {
 
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewpage);
+
+        tabLayout.setupWithViewPager(viewPager);
+        VPadapter vPadapter = new VPadapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vPadapter.addFragment(new LoginFragment(),"Login");
+        vPadapter.addFragment(new RegisterFragment(),"Register");
+        viewPager.setAdapter(vPadapter);
 
 
     }

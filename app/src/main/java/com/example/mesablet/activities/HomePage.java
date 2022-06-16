b40e2583cc8b780f2d45e8b141b9178779f0dab3
koralduel <koralduel3112@gmail.com>
2022-06-16 12:28:14 +0300

    add new nav bar to profile page

--- app/src/main/java/com/example/mesablet/activities/HomePage.java
+++ app/src/main/java/com/example/mesablet/activities/HomePage.java
@@ -3,12 +3,9 @@ package com.example.mesablet.activities;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
@@ -20,11 +17,6 @@ import com.example.mesablet.viewmodels.PostsViewModel;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;

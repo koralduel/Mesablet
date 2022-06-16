9b8de259c9d7724abda949513616a02cef3d2109
koralduel <koralduel3112@gmail.com>
2022-06-16 16:46:15 +0300

    start UI of specific chat

---
+++ app/src/main/java/com/example/mesablet/activities/MessagePage.java
@@ -0,0 +1,14 @@
+package com.example.mesablet.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MessagePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message_page);
+    }
+}
